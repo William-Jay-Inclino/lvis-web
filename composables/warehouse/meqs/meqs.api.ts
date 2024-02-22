b8267d109bb9114fc1b4b0a8e5878fc11e06c72b
William Jay Inclino <wjay.inclino@gmail.com>
2024-02-22 20:25:53 +0800

    initial ui for search meqs module

--- composables/warehouse/meqs/meqs.api.ts
+++ composables/warehouse/meqs/meqs.api.ts
@@ -1,73 +1,75 @@
-// import type { RV } from "../rv/rv.types";
-// import type { MEQS } from "./meqs.types";
+import type { RV } from "../rv/rv.types";
+import type { MEQS } from "./meqs.types";
 
 
-// export async function fetchDataInSearchFilters(): Promise<{
-//     meqs: MEQS[],
-//     employees: Employee[],
-//     rvs: RV[],
-// }> {
-//     const query = `
-//         query {
-//             canvasses(page: 1, pageSize: 10) {
-//                 data{
-//                     rc_number
-//                 }
-//             },
-//             rvs(page: 1, pageSize: 10) {
-//                 data{
-//                     rv_number
-//                 }
-//             }
-//             employees(page: 1, pageSize: 50) {
-//                 data {
-//                     id
-//                     firstname
-//                     middlename
-//                     lastname
-//                 }
-//             }
-//         }
-//     `;
+export async function fetchDataInSearchFilters(): Promise<{
+    meqs: MEQS[],
+    employees: Employee[],
+    rvs: RV[],
+}> {
+    const query = `
+        query {
+            meqs(page: 1, pageSize: 10) {
+                data{
+                    meqs_number
+                }
+            },
+            rvs(page: 1, pageSize: 10) {
+                data{
+                    rv_number
+                }
+            }
+            employees(page: 1, pageSize: 50) {
+                data {
+                    id
+                    firstname
+                    middlename
+                    lastname
+                }
+            }
+        }
+    `;
 
-//     try {
-//         const response = await sendRequest(query);
-//         console.log('response', response)
+    try {
+        const response = await sendRequest(query);
+        console.log('response', response)
 
-//         let canvasses = []
-//         let rvs = []
-//         let employees = []
+        let meqs = []
+        let rvs = []
+        let employees = []
 
-//         if(!response.data || !response.data.data) {
-//             throw new Error(JSON.stringify(response.data.errors));
-//         }
+        if(!response.data || !response.data.data) {
+            throw new Error(JSON.stringify(response.data.errors));
+        }
 
-//         const data = response.data.data
+        const data = response.data.data
 
-//         if(data.employees && data.employees.data) {
-//             employees = response.data.data.employees.data
-//         }
+        console.log('data', data)
 
-//         if(data.canvasses && data.canvasses.data) { 
-//             canvasses = data.canvasses.data
-//         }
+        if(data.employees && data.employees.data) {
+            employees = data.employees.data
+        }
 
-//         if(data.rvs && data.rvs.data) { 
-//             rvs = data.rvs.data
-//         }
+        if(data.meqs && data.meqs.data) { 
+            meqs = data.meqs.data
+        }
 
-//         return {
-//             canvasses,
-//             employees,
-//             rvs
-//         }
+        if(data.rvs && data.rvs.data) { 
+            rvs = data.rvs.data
+        }
 
-//     } catch (error) {
-//         console.error(error);
-//         return {
-//             canvasses: [],
-//             employees: [],
-//             rvs: []
-//         }
-//     }
-// }
+        return {
+            meqs,
+            employees,
+            rvs
+        }
+
+    } catch (error) {
+        console.error(error);
+        return {
+            meqs: [],
+            employees: [],
+            rvs: []
+        }
+    }
+}
--- pages/warehouse/purchasing/meqs/index.vue
+++ pages/warehouse/purchasing/meqs/index.vue
@@ -8,31 +8,44 @@
             <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="mb-3">
                     <label class="form-label">MEQS Number</label>
-                    <!-- <client-only>
-                        <v-select :options="rvs" label="rv_number" v-model="rv"></v-select>
-                    </client-only> -->
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <div class="mb-3">
-                    <label class="form-label">Date</label>
-                    <!-- <client-only>
-                        <v-select :options="canvasses" label="rc_number" v-model="canvass"></v-select>
-                    </client-only> -->
+                    <client-only>
+                        <v-select :options="meqsArray" label="meqs_number" v-model="meqs"></v-select>
+                    </client-only>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="mb-3">
                     <label class="form-label">Transaction</label>
-                    <!-- <input v-model="date_requested" type="date" class="form-control"> -->
+                    <div class="row g-0">
+                        <div class="col-4">
+                            <client-only>
+                                <v-select :options="transactionTypes" v-model="transactionType" :clearable="false"></v-select>
+                            </client-only>
+                        </div>
+                        <div class="col-8">
+                            <client-only>
+                                <v-select :options="rvs" label="rv_number" v-model="rv" v-show="transactionType === 'RV'"></v-select>
+                                <v-select :options="jos" label="rv_number" v-model="jo" v-show="transactionType === 'SPR'"></v-select>
+                                <v-select :options="sprs" label="rv_number" v-model="spr" v-show="transactionType === 'JO'"></v-select>
+                            </client-only>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="mb-3">
+                    <label class="form-label">Date</label>
+                    <input v-model="date_requested" type="date" class="form-control">
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="mb-3">
                     <label class="form-label">Requisitioner</label>
-                    <!-- <client-only>
+                    <client-only>
                         <v-select :options="employees" label="fullname" v-model="requested_by"></v-select>
-                    </client-only> -->
+                    </client-only>
                 </div>
             </div>
         </div>
@@ -43,9 +56,63 @@
 
 
 <script setup lang="ts">
+    import type { MEQS } from '~/composables/warehouse/meqs/meqs.types';
+    import type { RV } from '~/composables/warehouse/rv/rv.types';
+    import * as meqsApi from '~/composables/warehouse/meqs/meqs.api'
+
 
     definePageMeta({
             layout: "layout-admin"
     })
+
+
+    const isMobile = ref(false)
+
+
+    const transactionTypes = ref(['RV', 'SPR', 'JO'])
+    
+    // ====== search filters ====== 
+
+    // dropdowns
+    const meqsArray = ref<MEQS[]>([])
+    const rvs = ref<RV[]>([])
+    const jos = ref([])
+    const sprs = ref([])
+    const employees = ref<Employee[]>([])
+
+    // fields
+    const transactionType = ref(transactionTypes.value[0])
+    const meqs = ref<MEQS | null>(null)
+    const rv = ref<RV | null>(null)
+    const jo = ref(null)
+    const spr = ref(null)
+    const date_requested = ref(null)
+    const requested_by = ref<Employee | null>(null)
+    // ----------------
+
+    onMounted( async() => {
+        isMobile.value = window.innerWidth < MOBILE_WIDTH
+
+        window.addEventListener('resize', checkMobile);
+
+        const response = await meqsApi.fetchDataInSearchFilters()
+
+        meqsArray.value = response.meqs
+        rvs.value = response.rvs
+        employees.value = response.employees.map((i) => {
+            i.fullname = getFullname(i.firstname, i.middlename, i.lastname)
+            return i
+        })
+
+    })
+
+
+
+    // ======================== UTILS ======================== 
+
+    function checkMobile() {
+        isMobile.value = window.innerWidth < MOBILE_WIDTH
+    }
+
 
 </script>
