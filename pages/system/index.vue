75b11f70b69ebd4124551669b53c922b08335e86
jay <wjay.inclino@gmail.com>
2024-04-18 07:56:22 +0800

    UI update for system and warehouse index

--- pages/system/index.vue
+++ pages/system/index.vue
@@ -1,6 +1,29 @@
 <template>
-    <div> System Index </div>
+    
+    <div v-if="authUser" class="background-container">
+        
+        <div class="background-overlay"></div>
+
+        <div class="container border border-info">
+
+            <h1 class="text-white mt-5"> WELCOME TO SYSTEM MANAGEMENT</h1>
+
+            <div class="row text-white mb-5 mt-4">
+                <div class="col-lg-6 col-md-6 col-12">
+                    "Welcome to our System Management! Here, you can fine-tune your settings, define system variables, manage user permissions, and oversee data management. Empower your system's capabilities and tailor it to your unique needs."
+                    <br />
+                    <br />
+                    -Joshua Tayag
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
 </template>
+
 
 
 <script setup lang="ts">
@@ -9,4 +32,39 @@ definePageMeta({
     layout: "layout-system"
 })
 
+
+const authUser = ref<AuthUser>()
+
+onMounted(() => {
+    authUser.value = getAuthUser()
+})
+
+
+
 </script>
+
+
+
+<style scoped>
+
+.background-container {
+    /* display: grid;
+    place-items: center; */
+    height: 100vh;
+    overflow: auto;
+    padding-top: 40px;
+}
+
+.background-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('~/assets/img/systembg.png');
+    background-size: cover;
+    background-position: center;
+    z-index: -1;
+}
+
+</style>
--- pages/warehouse/index.vue
+++ pages/warehouse/index.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>warehouse index</div>
+    
+    <div v-if="authUser" class="background-container">
+        
+        <div class="background-overlay"></div>
+
+        <div class="container border border-info">
+
+            <h1 class="text-white mt-5"> WELCOME TO WAREHOUSE MANAGEMENT</h1>
+
+            <div class="row text-white mb-5 mt-4">
+                <div class="col-lg-6 col-md-6 col-12">
+                    "Welcome to our Warehouse Management! Your hub for seamless purchasing, efficient warehousing, reliable motorpool management, and precise stock inventory control. Let's optimize your operations together!" 
+                    <br />
+                    <br />
+                    -Joseph Ken Estrera
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
 </template>
 
 
@@ -10,4 +32,39 @@ definePageMeta({
     layout: "layout-warehouse"
 })
 
+
+const authUser = ref<AuthUser>()
+
+onMounted(() => {
+    authUser.value = getAuthUser()
+})
+
+
+
 </script>
+
+
+
+<style scoped>
+
+.background-container {
+    /* display: grid;
+    place-items: center; */
+    height: 100vh;
+    overflow: auto;
+    padding-top: 40px;
+}
+
+.background-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('~/assets/img/warehousebg.png');
+    background-size: cover;
+    background-position: center;
+    z-index: -1;
+}
+
+</style>
