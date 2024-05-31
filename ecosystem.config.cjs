9215422c3c5cdfbd63d8e3f0d7b57300083ec599
William Jay Inclino <wjay.inclino@gmail.com>
2024-05-31 11:05:07 +0800

    set configuration for autostart app

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'lvis-web',
+            script: './scripts/start_app.sh',
+            cwd: __dirname,
+        },
+    ]
+};
---
+++ scripts/start_app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the pnpm command
+pnpm run preview
