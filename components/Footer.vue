5c24e818f1a0b165d8f7e9b8f31aa604167ab4ae
jay <wjay.inclino@gmail.com>
2024-06-19 12:37:21 +0800

    update footer UI

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 <footer :class="{'bg-white': !isHomeRoute || isMobile}" class="footer mt-auto pt-3 d-flex justify-content-center align-items-center">
-      <div class="text-center text-dark fw-bold" :style="{fontFamily: 'monospace', color: '#ffd700', fontSize: isMobile ? '12px' : '16px', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%'}">
+      <div class="text-center text-muted" :style="{fontFamily: 'monospace', color: '#ffd700', fontSize: isMobile ? '11px' : '12px', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%'}">
         <p>&copy; 2024 LVUMS <i class="fas fa-bolt text-warning"></i> | Developed by <a target="_blank" href="https://www.facebook.com/jewell.inclino">William Jay Inclino</a></p>
       </div>
     </footer>
